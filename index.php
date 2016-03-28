2bda4c95c24d08ce23bf7ceaf193e0d907e3f6a6
Jacob B. Cholewa <jbec@itu.dk>
2016-03-28 14:50:40 +0200

    Refactored index.php

--- index.php
+++ index.php
@@ -1,32 +1,20 @@
 <?php
+//Remove error reporting in final version!
 ini_set('display_errors', 1);
 error_reporting(~0);
 require_once("ssas.php");
-
-$ssas = new ssas;
-$ssas -> createUser("Jacob","1234");
-echo '</br>';
-echo $ssas -> login("Jacob","1234");
-
 ?>
 
+<?php include 'header.php'; ?>
+<?php if(!$ssas -> isUserLoggedIn()){ ?>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags always come first -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
-  </head>
-  <body>
     <h1>Hello, world!</h1>
+    <p>You are logged in!</p>
 
-    <!-- jQuery first, then Bootstrap JS. -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
-  </body>
-</html>
+<?php } else { ?>
+
+    <h1>Hello, world!</h1>
+    <a href="login.php>Login</a>
+
+<?php } ?>
+<?php include 'footer.php'; ?>
