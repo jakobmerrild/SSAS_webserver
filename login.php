553dafa5ef226714c93aa53452e0a8c673e1ba35
Jacob B. Cholewa <jbec@itu.dk>
2016-03-28 14:38:27 +0200

    Fixed layout issues with login.php

--- login.php
+++ login.php
@@ -1,47 +1,66 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(~0);
-require_once("ssas.php");
+	//TODO Remove error reporting in final version!
+	ini_set('display_errors', 1);
+	error_reporting(~0);
+	
+	//Getting ssas class
+	require_once("ssas.php");
+	$ssas = new ssas();
 
-$ssas = new ssas;
-if($ssas -> isUserLoggedIn()){
-	header('Location: index.php');
-	die();
-}
-else if(!empty($_POST)){
-	$loginSuccessful = $ssas -> login($_POST["username"],$_POST["password"]);
-	if($loginSuccessful){
-		header('Location: index.php');
-		die();
+	//If a POST occured, try to authenticate
+	if(isset($_POST['username']) && isset($_POST['password'])){
+		$result = $ssas -> login($_POST['username'],$_POST['password']);
 	}
-}
+
+	//If the user is already logged in, redirect to index.php
+	if($ssas -> isUserLoggedIn()){
+		header("Location: index.php");
+		exit();
+	}
 ?>
+
 <html>
 	<head>
-	<!-- Required meta tags always come first -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<!-- Required meta tags always come first -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
 	</head>
 	<body>
-		<form role="form">
-		  <div class="form-group">
-			<label for="username">Username:</label>
-			<input type="username" class="form-control" id="username">
-		  </div>
-		  <div class="form-group">
-			<label for="password">Password:</label>
-			<input type="password" class="form-control" id="password">
-		  </div>
-		  <button type="submit" class="btn btn-default">Log in</button>
-		</form>
-		<?php
-			if(isset($loginSuccessful) && !$loginSuccessful){
-				echo '<span class="error-msg">Wrong username or password.</span>'
-			}
-		?>
+		<div class="container">
+			<div class="row">
+			<div class="col-sm-4 col-sm-offset-4">
+			<form action="login.php" method="post">
+				<div class="form-group">
+					<label for="username">Username:</label>
+					<input 
+						id="username"
+						type="text" 
+						class="form-control"
+						name="username" 
+					>
+				</div>
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input 
+						id="password"
+						type="password" 
+						class="form-control" 
+						name="password"
+					>
+				</div>
+				<button type="submit" class="btn btn-default">Log in</button>
+			</form>
+<?php if(isset($result) && !$result){ ?>
+<div class="alert alert-danger" role="alert">
+  <strong>Oh snap!</strong> Wrong username or password!
+</div>
+<?php } ?>
+</div>
+</div>
+		</div>
 	</body>
 </html>
