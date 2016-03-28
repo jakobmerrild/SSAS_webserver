10713c64eab202c12afc5f09feed3dd2bf948691
Jacob B. Cholewa <jbec@itu.dk>
2016-03-28 13:06:08 +0200

    Cosmetic changes

--- ssas.php
+++ ssas.php
@@ -17,49 +17,62 @@ class ssas {
 
     function createUser($username, $password){
 
-	$salt = base64_encode(mcrypt_create_iv(22,MCRYPT_DEV_URANDOM));
-        $options = ['salt' => $salt];
-        $ench_password = password_hash($password, PASSWORD_BCRYPT, $options);
+        //Generates salt in base64 and password hash
+        $salt = base64_encode(mcrypt_create_iv(22,MCRYPT_DEV_URANDOM));
+        $ench_password = password_hash($password, PASSWORD_BCRYPT, ['salt' => $salt ]);
 
+        //Insert user into database
         if ($query = self::$mysqli -> prepare('INSERT INTO user(username,password,salt) VALUES (?,?,?)')){
             $query -> bind_param('sss', $username,$ench_password,$salt);
             $query -> execute();
+
+            //If exactly one row was affacted then we know that the user was inserted.
+            return $query -> affected_rows == 1;
         }
 
-        echo "username: " . $username;
-
+        return false;
     }
 
     function login($username, $password){
 
+        //Query to get the user salt
         if($query = self::$mysqli -> prepare('SELECT salt FROM user WHERE username = ?')){
             $query -> bind_param('s', $username);
             $query -> execute();
             $query -> store_result();
-            $query -> bind_result($salt);
-            $query -> fetch();
+
+            //If there is a result then there is a salt
+            if($query -> num_rows > 0){
+                $query -> bind_result($salt);
+                $query -> fetch();
+            }
         }
 
+        //If a salt is set then we continue
         if(isset($salt)){
-            $options = ['salt' => $salt];
-            $hash = password_hash($password, PASSWORD_BCRYPT, $options);
+            //Generates password hash using the retrived salt
+            $hash = password_hash($password, PASSWORD_BCRYPT, ['salt' => $salt]);
 
+            //Querying the username and password combo
             if($query = self::$mysqli -> prepare('SELECT id FROM user WHERE username = ? AND password = ?')){
                 $query -> bind_param('ss', $username, $hash);
                 $query -> execute();
                 $query -> store_result();
-		if($query -> num_rows > 0){
-			$query -> bind_result($uid);
-		}
-                $query -> fetch();
+
+                //If there is a result then the login was successful
+                if($query -> num_rows > 0){
+                    $query -> bind_result($uid);
+                    $query -> fetch();
+                }
             }
 
+            //Setting the global userid - TODO SHOULD BE DONE WITH SESSION COOKIES INSTEAD!
             if(isset($uid)){
                 self::$uid = $uid;
-                return "ok";
+                return true;
             }
         }
-        return "error";
+        return false; 
     }
 }
 ?>
